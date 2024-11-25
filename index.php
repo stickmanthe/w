3829539cb3061ad7e5e198c98f7c51c24027292b
stickmanthe <89025036+stickmanthe@users.noreply.github.com>
2024-11-25 17:34:43 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="icon" href="gusli.png">
+    <meta name="viewport" content="width=device-width,initial-scale=1"/>
+    <meta charset="UTF-8"/>
+    <title>стикман Сайт</title>
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+
+</head>
+<body style="font-size: 150%; padding: 20px; background-color: black; font-family: 'Roboto', sans-serif; font-optical-sizing: auto; font-style: normal; color: white;">	
+<div style="padding: 20px; background: radial-gradient(#001EA4, #061D7D);">
+<a href="https://lf45p.pw/" style="text-decoration: none;"><span style="background-color: brown; padding: 10px; color: white;">Стикман сайт</span></a><br>
+<br><br>
+<p>Привет я стикман и добро пожаловать на&nbsp; мой&nbsp; сайт</p>
+<br>
+<hr>
+<br>
+<b><p style="padding: 20px; background-color: purple; color: white; width: 90%;">Новости сайта!!! :</p></b>
+<p>&emsp;<b><a href="https://lf45p.pw/articles/5" style="color: white;">Обновление сайта по содержанию и скоро будет новый домен!!!</a></b> (2024-10-28)</p>
+<p>&emsp;<b><a href="https://lf45p.pw/articles/4" style="color: white;">Обновление сайта</a></b> (2024-05-19)</p>
+<p>&emsp;<b><a href="https://lf45p.pw/articles/3" style="color: white;">Сайту плохо</a></b> (2024-04-15)</p>
+<p>&emsp;<b><a href="https://lf45p.pw/articles/2" style="color: white;">мистер тыква на русском языке</a></b> (2024-03-24)</p>
+<p>&emsp;<b><a href="https://lf45p.pw/articles/1" style="color: white;">Мой сайт работает</a></b> (2024-02-20)</p>
+<br>
+<hr>
+<br>
+<b><p style="padding: 20px; background-color: grey; color: white; width: 90%;">Статьи!!! :</p></b>
+<p>&emsp;<b><a href="https://lf45p.pw/articles/opengl-glfw-text-rendering-easy-nanovg" style="color: white;">OpenGL 3 GLFW Рендерим текст легко</a></b> (2024-10-28)</p>
+<br>
+<hr>
+<br>
+<p>Если что обращайтесь ко мне в мессенджерах да соц сетях</p>
+<p>discord:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a style="color: red;" href="https://discord.com/users/727103828800831549">stickmanvrobloxe<a/></p>
+<p>telegram:&nbsp;&nbsp; <a style="color: red;" href="https://t.me/stickmanvbobloxe">stickmanvbobloxe<a/></p>
+<p>&nbsp;</p>
+<p>twitter:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a style="color: red;" href="https://x.com/stickmanbobloxe">stickmanbobloxe<a/></p>
+<p>&nbsp;</p>
+<p>github:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a style="color: red;" href="https://github.com/stickmanthe">stickmanthe<a/></p>
+<p>&nbsp;</p>
+<center><p style="color: gray;">стикман в боблоксе 2024</p></center>
+</body>
+</html>
